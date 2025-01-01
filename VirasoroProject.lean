1b2ba020ffef4bcd8af6491477bca33ce103b95f
Kalle <kalle.kytola@aalto.fi>
2025-01-01 21:08:47 +0200

    Also add the main file to check the build.

---
+++ VirasoroProject.lean
@@ -0,0 +1,11 @@
+-- This module serves as the root of the `VirasoroProject` library.
+-- Import modules here that should be built as part of the library.
+import «VirasoroProject».CentralExtension
+import «VirasoroProject».CyclicTripleSum
+import «VirasoroProject».HeisenbergAlgebra
+import «VirasoroProject».IsCentralExtension
+import «VirasoroProject».LieCohomologySmallDegree
+import «VirasoroProject».VirasoroAlgebra
+import «VirasoroProject».VirasoroCocycle
+import «VirasoroProject».WittAlgebra
+import «VirasoroProject».WittAlgebraCohomology
