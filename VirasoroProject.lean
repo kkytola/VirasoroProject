0ead0dc5b6de68ad3891bc78d31ff1a13bc53813
Kalle <kalle.kytola@aalto.fi>
2025-07-12 00:45:21 +0300

    Try a fresh start to fix the broken setup.

--- VirasoroProject.lean
+++ VirasoroProject.lean
@@ -2,11 +2,13 @@
 -- Import modules here that should be built as part of the library.
 import «VirasoroProject».CentralExtension
 import «VirasoroProject».CyclicTripleSum
+import «VirasoroProject».FockSpace
 import «VirasoroProject».HeisenbergAlgebra
 import «VirasoroProject».IsCentralExtension
 import «VirasoroProject».LieCohomologySmallDegree
 import «VirasoroProject».SectionSES
 import «VirasoroProject».Sugawara
+import «VirasoroProject».VermaModule
 import «VirasoroProject».VirasoroAlgebra
 import «VirasoroProject».VirasoroCocycle
 import «VirasoroProject».WittAlgebra
